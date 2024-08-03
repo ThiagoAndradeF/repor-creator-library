ad2b0fe54586d02e5bf035e36064b1f2fe318dae
Thiago Andrade <thiagoandrade160302@gmail.com>
2024-08-03 00:45:59 -0300

    return word in array bytes

--- htmlToWordConverter.cs
+++ htmlToWordConverter.cs
@@ -1,17 +1,23 @@
+using System.IO;
 using Xceed.Words.NET;
 
 public class HtmlToWordConverter
 {
-    public void ConvertHtmlToWord(string html, string filePath)
-    {     
-        using (var document = DocX.Create(filePath))
+    public byte[] ConvertHtmlToWord(string html)
+    {
+        using (var memoryStream = new MemoryStream())
         {
-            var paragraph = document.InsertParagraph();
-            paragraph.Font("Arial");
-            paragraph.FontSize(12);
-            paragraph.SpacingAfter(10);
-            paragraph.AppendLine(html); 
-            document.Save();
+            using (var document = DocX.Create(memoryStream))
+            {
+                var paragraph = document.InsertParagraph();
+                paragraph.Font("Arial");
+                paragraph.FontSize(12);
+                paragraph.SpacingAfter(10);
+                paragraph.AppendLine(html); 
+                document.Save();
+                memoryStream.Position = 0;
+                return memoryStream.ToArray();
+            }
         }
     }
 }
